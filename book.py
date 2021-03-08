60e65dd4fb90551d61affb1f5113cebea3af8b82
NonoLmx <nordine.lmx@hotmail.com>
2021-03-08 22:37:39 +0100

    modification of insert buy/sell order methods

--- book.py
+++ book.py
@@ -4,17 +4,28 @@
 
 ### Order Class
 class Order:
-
-    global id
+    
     id = 1
     ##Constructor :
-    def __init__(self, quantity, price, side, id = 1): 
-        self.__quantity = quantity
-        self.__price = price
-        self.__id = id 
-        id = id + 1
-        #self.id_order = self.id_order + 1 #at each creation of an order the id is incremented by 1
+    def __init__(self, quantity, price, side):
+        self.__quantity = quantity          #Volume of the order
+        self.__price = price                #Price of the order
+        self.__id = Order.id                #Id of each order
+        Order.id = Order.id + 1             #at each creation of an order the id is incremented by 1
+        
+    ##Property :
 
+    @property
+    def quantity(self):
+        return self.__quantity
+    
+    @property
+    def price(self):
+        return self.__price
+
+    @property
+    def Id(self):
+        return self.__id
 
     ##Methods :
 
@@ -43,30 +54,62 @@ class Book:
         self.__name = name              #Book name
         self.__list_sell_order = []     #list of sell order
         self.__list_buy_order = []      #list of buy order
-        self.__id = 1                   #Id of each order
+              
+
+    ##Property :
         
+    @property
+    def name(self):
+        return self.__name 
+
+    @property
+    def list_sell_order(self):
+        return self.__list_sell_order
+
+    @property
+    def list_buy_order(self):
+        return self.__list_sell_order
+
+      
     ##Methods :
 
 
     #Overladed Methods
     def __str__(self): # human-readable content
-        return  "Order Book %s : %s" % (self.__name, [repr(self.__list_sell_order[i]) for i in reversed(range(len(self.__list_sell_order)))])
+        return  "Order Book %s : \n ------------------------- \n ASK %s \n BID %s" % (self.__name, self.__list_sell_order, self.__list_buy_order)
 
     #Insert sell order :
+    def insert_sell2(self, quantity, price):
+        ##Creation of the order :
+        order = Order(quantity, price, side = "sell")
+
+        ##Insertion of the order (condition):
+
+        #if it is the first order of the book :
+        if len(self.__list_sell_order) == 0 :
+            self.__list_sell_order.append(order)
+            
+        
+        else :
+            for i in range(len(self.__list_sell_order)):
+
+                if order < self.__list_buy_order[i] : 
+                    self.__list_sell_order.insert(i, order)
+                
+                    break
+
     def insert_sell(self, quantity, price):
         ##Creation of the order :
         order = Order(quantity, price, side = "sell")
 
         ##Insertion of the order (condition):
-        for i in range(len(self.__list_sell_order)):
+        self.__list_sell_order.append(order)
+        self.__list_sell_order.sort()
+        
 
-            if order < self.__list_buy_order[i] : 
-                self.__list_sell_order.insert(i, order)
-                
-                break
     
     #Insert buy order :
-    def insert_buy(self, quantity, price):
+    def insert_buy2(self, quantity, price):
         ##Creation of the order :
         order = Order(quantity, price, side = "buy")
 
@@ -78,8 +121,14 @@ class Book:
                 
                 break
 
-    def show(self):
-        for i in range(len(self.__list_sell_order)):
-            repr(self.__list_sell_order[i])
+    def insert_buy(self, quantity, price):
+        ##Creation of the order :
+        order = Order(quantity, price, side = "buy")
+
+        ##Insertion of the order (condition):
+        self.__list_buy_order.append(order)
+        self.__list_buy_order.sort(reverse = True)
+
+
         
 
