a0ae22d5176b73ab03d62cd9d6edcf915a11bad4
kadir69200 <kayakadir71@gmail.com>
2021-03-12 10:05:30 +0100

    last version

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-p3 install virtualenv -- force - reinstall
+pip3 install virtualenv --force-reinstall
 python3 -m venv .v
-source .v/ bin / activate
+source .v/bin/activate
 python3 -m pip install -r requirements.txt
 python3 main.py
